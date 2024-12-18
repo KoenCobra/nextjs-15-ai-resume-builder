d9bf843d053e5ac93346fc84b0264a401583ab8a
Koen De Groot <koendegroot84@gmail.com>
2024-12-18 20:36:42 +0100

    resumes query

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,4 +1,7 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
+import { resumeDataInclude } from "@/lib/types";
+import { auth } from "@clerk/nextjs/server";
 import { PlusSquare } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
@@ -8,7 +11,22 @@ export const metadata: Metadata = {
   title: "Your resumes",
 };
 
-const Resumes = () => {
+const Resumes = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+
+  const [resumes, totalCount] = await Promise.all([
+    prisma.resume.findMany({
+      where: { userId },
+      orderBy: { updatedAt: "desc" },
+      include: resumeDataInclude,
+    }),
+    prisma.resume.count({ where: { userId } }),
+  ]);
+
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
       <Link href="/editor" className="mx-auto flex w-fit gap-2">
